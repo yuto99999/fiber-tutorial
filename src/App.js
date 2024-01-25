e62784d742d663057725dae5548a78439cbfe75b
yuto99999 <myuto2003@gmail.com>
2024-01-25 17:23:29 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { config, useSpring, animated } from "@react-spring/three";
+
+function Box(props) {
+  const ref = useRef();
+  const [clicked, setClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+
+  useFrame(() => (ref.current.rotation.x += 0.01));
+
+  const { scale } = useSpring({
+    scale: clicked ? 2 : 1,
+    config: config.wobbly,
+  });
+  return (
+    <animated.mesh
+      {...props}
+      ref={ref}
+      onClick={() => setClicked(!clicked)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      scale={scale}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial
+        color={hovered ? "rgb(193,174,244)" : "rgb(174 , 214, 244)"}
+      />
+    </animated.mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          <Box position={[-1.6, 0, 0]} />
+          <Box position={[1.6, 0, 0]} />
+          <ambientLight intensity={0.75} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+        </Canvas>
+      </div>
+      <h1>Threejs Fiber</h1>
+    </>
   );
 }
 
